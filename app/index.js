b3ce3d936bef98cf47f54a7ab43d8bbbee48afb7
htmlucas <lucas.emanoell@outlook.com>
2018-03-26 16:00:17 -0300

    Adiconada Ferramenta de Buttons

--- app/index.js
+++ app/index.js
@@ -4,8 +4,12 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
 
 EStyleSheet.build({
-   $primaryBlue: '#4F6D7A',
-  $white: '#FFFFFF',
+    $primaryBlue: '#4F6D7A',
+    $white: '#FFFFFF',
+    $lightGray: '#F0F0F0',
+    $border: '#979797',
+    $inputText: '#797979',
 });
  
 export default () => <Home />;
+

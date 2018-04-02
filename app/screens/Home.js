1ecba04af88a88ca6b85f4b9d0e1cc980fc93fef
htmlucas <lucas.emanoell@outlook.com>
2018-04-02 15:42:43 -0300

    Adicionando a funcionalidade de Header

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { StatusBar, KeyboardAvoidingView } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Button';
 import { LastConverted } from '../components/Text';
- 
+import { Header } from '../components/Header';
+
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
 const TEMP_LAST_CONVERTED = new Date();
 const TEMP_CONVERSION_RATE = 0.79739;
- 
+
 class Home extends Component {
   handleChangeText = () => {
     console.log('change text');
@@ -31,10 +32,15 @@ class Home extends Component {
     console.log('clear button pressed');
   };
 
+  handleOptionsPress = () => {
+    console.log('options press');
+  };
+
   render() {
     return (
       <Container>
         <StatusBar backgroundColor="blue" barStyle="light-content" />
+        <Header onPress={this.handleOptionsPress} />
         <Logo />
         <InputWithButton
           buttonText={TEMP_BASE_CURRENCY}
@@ -55,11 +61,13 @@ class Home extends Component {
           quote={TEMP_QUOTE_CURRENCY}
           conversionRate={TEMP_CONVERSION_RATE}
         />
-        <ClearButton onPress={this.handleSwapCurrency} text="Reverse Currencies" />
+        <ClearButton text="clear" onPress={this.handle} />
       </Container>
     );
   }
 }
 
 export default Home;
+
+
 

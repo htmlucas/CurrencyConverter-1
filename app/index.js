606173ec5cf276d9e66e0825fba1647570a15274
htmlucas <lucas.emanoell@outlook.com>
2018-04-09 15:55:53 -0300

    Adicionando as funcionalidades de Lista Flat, Lista de item, Tela de opções e Tela de Temas

--- app/index.js
+++ app/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
-    $primaryBlue: '#4F6D7A',
-    $white: '#FFFFFF',
-    $lightGray: '#F0F0F0',
-    $border: '#979797',
-    $inputText: '#797979',
+  $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+  $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#979797',
+  $inputText: '#797979',
+  $darkText: '#343434',
 });
- 
-export default () => <Home />;
+
+export default () => <Themes />;
 
